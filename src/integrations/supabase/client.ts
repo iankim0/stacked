349af72fef0a232e6b7029bb504ff1bc291658ba
Ian Kim <iank0426@gmail.com>
2025-08-10 17:30:09 -0400

    Refactor for security

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
+import dotenv from 'dotenv';
+console.log(import.meta.env.VITE_SUPABASE_URL);
+console.log(import.meta.env.VITE_SUPABASE_PUBLIC_ANON_KEY);
 
-const SUPABASE_URL = "https://vrstutpoctffnqltlbym.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyc3R1dHBvY3RmZm5xbHRsYnltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2NjM1OTYsImV4cCI6MjA2OTIzOTU5Nn0.zqED032IqAIrv61lR2ftYAkY5_jeBfk59af4q-PROrs";
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLIC_ANON_KEY;
+// const SUPABASE_URL = process.env.NEXT_SUPABASE_URL;
+// const SUPABASE_PUBLISHABLE_KEY = process.env.NEXT_SUPABASE_PUBLIC_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
